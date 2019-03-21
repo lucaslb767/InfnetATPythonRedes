24f32402c735456d8bca2d32348661abc0bdf840
Lucas Barrozo <lucaslbarrozo@gmail.com>
2019-03-21 17:42:39 -0300

    Q01

--- Q01.py
+++ Q01.py
@@ -1,23 +1,26 @@
 '''
 Escreva um programa em Python que:
-obtenha a lista de processos executando no momento, considerando que o processo pode deixar de existir enquanto seu programa manipula suas informações;
+obtenha a lista de processos executando no momento, considerando que o processo pode deixar de existir enquanto seu
+programa manipula suas informações;
+
 imprima o nome do processo e seu PID;
 imprima também o percentual de uso de CPU e de uso de memória.
 '''
 
-import os
-import psutil
-import pprint
+import psutil , pprint
 
-print(psutil.pids())
+lista_process =[]
+try:
+    for proc in psutil.process_iter():
+        lista_process.append([proc.pid, proc.name()])
 
+except Exception as e:
+    print(f'Ao tentar capturar algum processo, o seguinte erro ocorreu: {e}')
 
+print('LISTA PROCESSOS')
+pprint.pprint(lista_process)
+mem_virtual = psutil.virtual_memory()
+mem_percent = mem_virtual.used / mem_virtual.total
+print(f'A porcentagem de uso de CPU é : {psutil.cpu_percent()}%')
+print(f'A porcentagem de uso de Ram é: {round(mem_percent*100,2)}% ')
 
-process_names = []
-process_pid = psutil.pids()
-for pid in process_pid:
-
-    name = psutil.Process(pid)
-    process_names.append(name)
-
-print(process_names)
