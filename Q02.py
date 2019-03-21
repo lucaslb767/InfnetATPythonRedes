2fd78a749536e0c8ed57a48377b36cff09d91d39
Lucas Barrozo <lucaslbarrozo@gmail.com>
2019-03-21 18:18:29 -0300

    Q02

--- Q02.py
+++ Q02.py
@@ -2,3 +2,9 @@
 Escreva um programa que obtenha um nome de um arquivo texto do usuário e crie um processo para executar o programa do sistema Windows
 bloco de notas (notepad) para abrir o arquivo.
 '''
+
+import subprocess
+
+nome_do_arquivo = input('Digite o nome do arquivo e sua extensão (arquivo.txt) ')
+
+subprocess.Popen(['notepad', 'Q02.txt'])
--- Q03.py
+++ Q03.py
@@ -1,6 +1,10 @@
 '''
 Escreva um programa em Python que:
-gere uma estrutura que armazena o nome dos arquivos em um determinado diretório e a quantidade de bytes que eles ocupam em disco. Obtenha o nome do diretório do usuário.
+gere uma estrutura que armazena o nome dos arquivos em um determinado diretório e a quantidade de bytes que eles ocupam
+em disco. Obtenha o nome do diretório do usuário.
+
+
 Ordene decrescentemente esta estrutura pelo valor da quantidade de bytes ocupada em disco (pode usar as funções sort ou sorted);
 gere um arquivo texto com os valores desta estrutura ordenados.
 '''
+
