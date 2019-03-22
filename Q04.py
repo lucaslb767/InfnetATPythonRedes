be7888fa5bab460b7b56d26f6aafd1a673f9958f
Lucas Barrozo <lucaslbarrozo@gmail.com>
2019-03-21 22:29:47 -0300

    Q04

--- Q04.py
+++ Q04.py
@@ -11,3 +11,15 @@ Resultado na tela:
 
 aid moB
 '''
+
+texto = open('Q04.txt', 'r')
+
+linhas = texto.readlines()
+
+for n in range(len(linhas)-1,-1,-1):
+
+    if '\\n' in linhas[n]:
+        linhas[n].replace('\\n', '')
+
+    print(linhas[n][::-1])
+
